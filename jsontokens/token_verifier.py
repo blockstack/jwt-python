d820ea29c472df45567c091c282ba153f8803e9d
Jude Nelson <judecn@gmail.com>
2017-06-30 16:33:38 -0400

    add rudimentary support for verifying rfc 7515 JWTs with multiple public keys

--- jsontokens/token_verifier.py
+++ jsontokens/token_verifier.py
@@ -24,7 +24,12 @@ from .utils import (
 from .key_loading import load_verifying_key
 
 
-def unpack_token(token):
+def _unpack_token_compact(token):
+    """
+    Unpack a compact-form serialized JWT.
+    Returns (header, payload, signature, signing_input) on success
+    Raises DecodeError on bad input
+    """
     if isinstance(token, (str, unicode)):
         token = token.encode('utf-8')
 
@@ -65,14 +70,111 @@ def unpack_token(token):
     return (header, payload, signature, signing_input)
 
 
-def decode_token(token):
-    header, payload, raw_signature, signing_input = unpack_token(token)
+def _unpack_token_json(token):
+    """
+    Unpack a JSON-serialized JWT
+    Returns (headers, payload, signatures) on success
+    Raises DecodeError on bad input
+    """
+    if not isinstance(token, dict):
+        raise DecodeError("Not a dict")
+
+    if not token.has_key('payload'):
+        raise DecodeError("Missing 'payload' field")
+
+    for k in ['header', 'signature']:
+        if not token.has_key(k):
+            raise DecodeError("Missing '{}' field".format(k))
+
+        if not isinstance(token[k], list):
+            raise DecodeError("Field '{}' is not a string".format(k))
+
+    headers = []
+    signatures = []
+    signing_inputs = []
+    payload = None
+
+    try:
+        headers = [base64url_decode(h) for h in token['header']]
+    except (TypeError, binascii.Error):
+        raise DecodeError("Invalid header padding")
+
+    try:
+        payload_data = base64url_decode(token['payload'])
+    except (TypeError, binascii.Error):
+        raise DecodeError("Invalid payload padding")
+
+    try:
+        payload = json.loads(payload_data.decode('utf-8'))
+    except ValueError as e:
+        raise DecodeError('Invalid payload string: {}'.format(e))
+
+    try:
+        signatures = [base64url_decode(s) for s in token['signature']]
+    except (TypeError, binascii.Error):
+        raise DecodeError("Invalid crypto padding")
+
+    for header_b64 in token['header']:
+        signing_inputs.append( b'{}.{}'.format(header_b64, token['payload']) )
+
+    return (headers, payload, signatures, signing_inputs)
+
+
+def unpack_token(token):
+    """
+    Unpack a JSON-serialized JWT or a compact-serialized JWT
+    Returns (header, payload, signature, signing_input) on success
+    Raises DecodeError on bad input
+    """
+    if isinstance(token, (unicode,str)):
+        return _decode_token_compact(token)
+
+    else:
+        return _decode_token_json(token)
+
+
+def _decode_token_compact(token):
+    """
+    Decode a compact-serialized JWT
+    Returns {'header': ..., 'payload': ..., 'signature': ...}
+    """
+    header, payload, raw_signature, signing_input = _unpack_token_compact(token)
     token = { 
         "header": header,
         "payload": payload,
         "signature": base64url_encode(raw_signature)
     }
     return token
+
+
+def _decode_token_json(token):
+    """
+    Decode a JSON-serialized JWT
+    Returns {'header': ..., 'payload': ..., 'signature': ...}
+    """
+    header, payload, raw_signatures, signing_inputs = _unpack_token_json(token)
+    token = {
+        'header': header,
+        'payload': payload,
+        'signature': [base64url_encode(rs) for rs in raw_signatures]
+    }
+    return token
+
+
+def decode_token(token):
+    """
+    Top-level method to decode a JWT.
+    Takes either a compact-encoded JWT with a single signature,
+    or a multi-sig JWT in the JSON-serialized format.
+
+    Returns the deserialized token, as a dict.
+    The signatures will still be base64-encoded
+    """
+    if isinstance(token, (unicode,str)):
+        return _decode_token_compact(token)
+
+    else:
+        return _decode_token_json(token)
 
 
 class TokenVerifier():
@@ -85,22 +187,26 @@ class TokenVerifier():
     def _get_verifier(self, verifying_key, signature):
         return verifying_key.verifier(signature, self.signing_function)
 
-    @classmethod
-    def _unpack(cls, token):
-        return unpack_token(token)
-
-    def verify(self, token, verifying_key):
+    def _verify_single(self, token, verifying_key):
+        """
+        Verify a compact-formatted JWT signed by a single key
+        Return True if authentic
+        Return False if not
+        """
         # grab the token parts
-        token_parts = self._unpack(token)
-        header, payload, raw_signature, signing_input = token_parts
+        header, payload, raw_signature, signing_input = _unpack_token_compact(token)
+
         # load the verifying key
         verifying_key = load_verifying_key(verifying_key, self.crypto_backend)
+        
         # convert the raw_signature to DER format
         der_signature = raw_to_der_signature(
             raw_signature, verifying_key.curve)
+        
         # initialize the verifier
         verifier = self._get_verifier(verifying_key, der_signature)
         verifier.update(signing_input)
+        
         # check to see whether the signature is valid
         try:
             verifier.verify()
@@ -108,6 +214,72 @@ class TokenVerifier():
             # raise DecodeError('Signature verification failed')
             return False
         return True
+
+
+    def _verify_multi(self, token, verifying_keys, num_required=None):
+        """
+        Verify a JSON-formatted JWT signed by multiple keys is authentic.
+        Optionally set a threshold of required valid signatures with num_required.
+        Return True if valid
+        Return False if not
+
+        TODO: support multiple types of keys
+        """
+        headers, payload, raw_signatures, signing_inputs = _unpack_token_json(token)
+        if num_required is None:
+            num_required = len(raw_signatures)
+
+        if num_required > len(verifying_keys):
+            # not possible
+            return False
+
+        if len(headers) != len(raw_signatures):
+            # invalid 
+            raise DecodeError('Header/signature mismatch')
+
+        verifying_keys = [load_verifying_key(vk, self.crypto_backend) for vk in verifying_keys]
+
+        # sanity check: only support one type of key :(
+        for vk in verifying_keys:
+            if vk.curve.name != verifying_keys[0].curve.name:
+                raise DecodeError("TODO: only support using keys from one curve per JWT")
+                
+        der_signatures = [raw_to_der_signature(rs, verifying_keys[0].curve) for rs in raw_signatures]
+
+        # verify until threshold is met
+        num_verified = 0
+        for (signing_input, der_sig) in zip(signing_inputs, der_signatures):
+            for vk in verifying_keys:
+                verifier = self._get_verifier(vk, der_sig)
+                verifier.update(signing_input)
+                try:
+                    verifier.verify()
+                    num_verified += 1
+                    verifying_keys.remove(vk)
+                    break
+                except InvalidSignature:
+                    pass
+
+            if num_verified >= num_required:
+                break
+
+        return (num_verified >= num_required)
+
+    
+    def verify(self, token, verifying_key_or_keys, num_required=None):
+        """
+        Verify a compact-formated JWT or a JSON-formatted JWT signed by multiple keys.
+        Return True if valid
+        Return False if not valid
+
+        TODO: support multiple types of keys
+        """
+        if isinstance(verifying_key_or_keys, list):
+            return self._verify_multi(token, verifying_key_or_keys, num_required=num_required)
+
+        else:
+            return self._verify_single(token, verifying_key_or_keys)
+        
 
     @classmethod
     def decode(cls, token):
