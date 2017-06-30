7c268732d3edcd31c763ebcc4b85ef66d6633acb
Jude Nelson <judecn@gmail.com>
2017-06-30 16:33:59 -0400

    simply copy over is_hex() from utilitybelt (removes a big import)

--- jsontokens/utils.py
+++ jsontokens/utils.py
@@ -15,6 +15,29 @@ from cryptography.hazmat.primitives.asymmetric.utils import (
 )
 
 
+def hex_to_int(s):
+    try:
+        return int(s, 16)
+    except:
+        raise ValueError("Value must be in hex format")
+
+
+def is_hex(s):
+    # make sure that s is a string
+    if not isinstance(s, str):
+        return False
+    # if there's a leading hex string indicator, strip it
+    if s[0:2] == '0x':
+        s = s[2:]
+    # try to cast the string as an int
+    try:
+        i = hex_to_int(s)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 def json_encode(input):
     return json.dumps(input, separators=(',', ':')).encode('utf-8')
 
