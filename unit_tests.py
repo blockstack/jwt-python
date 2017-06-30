47f362fc84f5fc491de703d383d3e14ad5f7e31c
Jude Nelson <judecn@gmail.com>
2017-06-30 16:34:18 -0400

    add unit tests for multi-sig rfc 7515 JWTs

--- unit_tests.py
+++ unit_tests.py
@@ -13,6 +13,14 @@ class MainTests(unittest.TestCase):
         self.public_key_hex = '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69'
         self.sample_payload = {"issuedAt": "1440713414.19"}
 
+        self.multiple_private_keys = [
+            '99eca4792e019686f2b115d65224c72431d1dc361a1bacbf46daf0ed194ea07c01', 
+            '43baf5d14516a295f8883425bb0b35de7e08ab126965c5775cf7a5956d2eb6b801'
+        ]
+        self.multiple_public_keys = [ECPrivateKey(pk).public_key().to_hex() for pk in self.multiple_private_keys]
+        self.multiple_invalid_public_keys_1 = [self.public_key_hex, self.multiple_public_keys[0]]
+        self.multiple_invalid_public_keys_2 = [self.public_key_hex, self.multiple_public_keys[1], self.multiple_public_keys[0]]
+
     def tearDown(self):
         pass
 
@@ -33,6 +41,45 @@ class MainTests(unittest.TestCase):
         token_is_valid = token_verifier.verify(token, self.public_key_hex)
         #print token_is_valid
         self.assertTrue(token_is_valid)
+
+
+    def test_signing_decoding_and_verifying_multikey(self):
+        token_signer = TokenSigner()
+        token = token_signer.sign(self.sample_payload, self.multiple_private_keys)
+        self.assertTrue(isinstance(token, dict))
+
+        decoded_token = TokenVerifier.decode(token)
+        self.assertTrue(isinstance(decoded_token, dict))
+        self.assertTrue(isinstance(decoded_token['header'], list))
+        self.assertTrue(isinstance(decoded_token['signature'], list))
+        self.assertTrue(len(decoded_token['header']) == 2)
+        self.assertTrue(len(decoded_token['signature']) == 2)
+
+        token_verifier = TokenVerifier()
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_public_keys)
+        )
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_public_keys, num_required=1)
+        )
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_invalid_public_keys_1, num_required=1)
+        )
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_invalid_public_keys_2)
+        )
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_invalid_public_keys_2, num_required=1)
+        )
+        self.assertTrue(
+            token_verifier.verify(token, self.multiple_invalid_public_keys_2, num_required=0)
+        )
+        self.assertFalse(
+            token_verifier.verify(token, self.multiple_invalid_public_keys_1)
+        )
+        self.assertFalse(
+            token_verifier.verify(token, self.multiple_public_keys, num_required=3)
+        )
 
 
 def test_main():
